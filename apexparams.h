0d9b347e9567f540d41c651e6b70b527cc2c9b2c
udit01 <uditjainiitd@gmail.com>
2018-10-11 17:04:26 +0530

    Added apex_params.h

---
+++ apexparams.h
@@ -0,0 +1,30 @@
+
+#ifndef _APEX_PARAMS_H_
+#define _APEX_PARAMS_H_
+
+// The FUSE API has been changed a number of times.  So, our code
+// needs to define the version of the API that we assume.  As of this
+// writing, the most current API version is 26
+#define FUSE_USE_VERSION 26
+
+// need this to get pwrite().  I have to use setvbuf() instead of
+// setlinebuf() later in consequence.
+#define _XOPEN_SOURCE 500
+
+#include <limits.h>
+#include <stdio.h>
+
+struct apex_data{
+    // This will contain the used, unused hastables
+    // Block params and factors and their calculations,
+    // Master file table, list etc
+};
+
+
+struct apex_state {
+    apex_data *data;
+    char *rootdir;
+};
+#define APEX_DATA ((struct apex_state *) fuse_get_context()->private_data)
+
+#endif
