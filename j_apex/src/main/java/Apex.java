237fa1ef747cb837bb3ee1f885a0698007bd0ef3
udit01 <uditjainiitd@gmail.com>
2018-10-18 19:14:53 +0530

    Added ApexDir class, but depending on block level consequences, we may have to remove it and make this FS only for files, meaning we can't create directory's ever. It shouldn't affect the performance analysis but simplify the implementation

--- j_apex/src/main/java/Apex.java
+++ j_apex/src/main/java/Apex.java
@@ -22,6 +22,8 @@ public class Apex extends FuseStubFS {
 
     // A class which has the common features of dir's and files
     abstract class ApexPath {
+        public
+
         String name;
         ApexDir parent;
 
@@ -65,7 +67,79 @@ public class Apex extends FuseStubFS {
 
     }
 
-    class ApexDir{
+    // Didn't think about dir's before, because it consisted solely of files
+    class ApexDir extends ApexPath{
+        public
+        //How will this directory's sub(path's) will be stored ?
+        // As a list for now // ApexPaths has both dir and files
+        List<ApexPath> contents = new ArrayList<>();
 
+        ApexDir(String name){ super(name);}
+        ApexDir(String name, ApexDir parent){super(name, parent);}
+
+        synchronized void add(ApexPath p){
+            contents.add(p);
+            p.parent = this;
+        }
+
+        synchronized void deleteChild(ApexPath child){ contents.remove(child);}
+
+        // depends on implementation of directory?
+        // recursively searching in 'apexpaths'
+        @Override
+        ApexPath find(String path) {
+            // We don't have to increase usage factors here
+            if (super.find(path) != null) {
+                return super.find(path);
+            }
+            while (path.startsWith("/")) {
+                path = path.substring(1);
+            }
+            synchronized (this) {
+                if (!path.contains("/")) {
+                    for (ApexPath p : contents) {
+                        if (p.name.equals(path)) {
+                            return p;
+                        }
+                    }
+                    return null;
+                }
+                String nextName = path.substring(0, path.indexOf("/"));
+                String rest = path.substring(path.indexOf("/"));
+                for (ApexPath p : contents) {
+                    if (p.name.equals(nextName)) {
+                        return p.find(rest);
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public void getattr(FileStat stat){
+            // Don't have to increase the usage factor here?
+            stat.st_mode.set(FileStat.S_IFDIR | 0777);
+            stat.st_uid.set(getContext().uid.get());
+            stat.st_gid.set(getContext().gid.get());
+        }
+
+        synchronized void mkdir(String lastComponent) {
+            contents.add(new ApexDir(lastComponent, this));
+        }
+
+        synchronized void mkfile(String lastComponent){
+            //incerase usage & hist factor of the file in constructor
+            contents.add(new ApexFile(lastComponent, this));
+        }
+
+        synchronized void read(Pointer buf, FuseFillDir filler){
+            for(ApexPath p : contents){
+                // didn't understand fully but may be used to display debug information
+                filler.apply(buf, p.name, null, 0);
+            }
+        }
+
+        // Delete method was already in the abstract class,
+        // could be used to change factors or deal with indivisual blocks etc
     }
 }
